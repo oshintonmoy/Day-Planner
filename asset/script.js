d92531ec1a1d9e5db4e8780d3783875a41fbe6b3
Mir Hasan <tonmoyhasan768@gmail.com>
2021-01-06 14:36:48 -0500

    , inital asset folder to the mian branch

--- asset/script.js
+++ asset/script.js
@@ -5,12 +5,19 @@ class TimeblockObj {
   }
 }
 
+
+
 window.onload = function() {
-  const currentTimeblocks = getCurrentTimeblocks();
-  const currentTime = moment();
+  var currentTimeblocks = getCurrentTimeblocks();
+  var currentTime = moment();
+
+
 
   displayCurrentDate(currentTime);
   displayTimeblockRows(currentTime);
+
+
+
 
   document.querySelector('.container')
     .addEventListener('click', function(event) {
@@ -19,58 +26,62 @@ window.onload = function() {
   setTimeblockText(currentTimeblocks);
 };
 
+
 function getCurrentTimeblocks() {
-  const currentTimeblocks = localStorage.getItem('timeblockObjects');
+ var currentTimeblocks = localStorage.getItem('timeblockObjects');
   return currentTimeblocks ? JSON.parse(currentTimeblocks) : [];
 }
+
+
 
 function displayCurrentDate(currentTime) {
   document.getElementById('currentDay')
     .textContent = currentTime.format('dddd, MMMM Do');
 }
 
+
 // functions for displaying all timeblock rows //
 function displayTimeblockRows(currentTime) {
-  const currentHour = currentTime.hour();
-  //working hours are 9-5 or 9-17
-  for (let i = 9; i <= 17; i ++) {
-    const timeblock = createTimeblockRow(i);
-    const hourCol = createCol(createHourDiv(i), 1);
-    const textArea = createCol(createTextArea(i, currentHour), 10);
-    const saveBtn = createCol(createSaveBtn(i), 1);
+  var currentHour = currentTime.hour();
+  //working hours are 8-4 or 8-16//
+  for (let i = 8; i <= 16; i ++) {
+    var timeblock = createTimeblockRow(i);
+    var hourCol = createCol(createHourDiv(i), 1);
+    var textArea = createCol(createTextArea(i, currentHour), 10);
+    var saveBtn = createCol(createSaveBtn(i), 1);
     appendTimeblockColumns(timeblock, hourCol, textArea, saveBtn);
     document.querySelector('.container').appendChild(timeblock);
   }
 }
 
 function createTimeblockRow(hourId) {
-  const timeblock = document.createElement('div');
+  var timeblock = document.createElement('div');
   timeblock.classList.add('row');
   timeblock.id = `timeblock-${hourId}`;
   return timeblock;
 }
 
 function createCol(element, colSize) {
-  const col = document.createElement('div');
+  var col = document.createElement('div');
   col.classList.add(`col-${colSize}`,'p-0');
   col.appendChild(element);
   return col;
 }
 
 function createHourDiv(hour) {
-  const hourCol = document.createElement('div');
+  var hourCol = document.createElement('div');
   hourCol.classList.add('hour');
   hourCol.textContent = formatHour(hour);
   return hourCol;
 }
 
 function formatHour(hour) {
-  const hourString = String(hour);
+  var hourString = String(hour);
   return moment(hourString, 'h').format('hA');
 }
 
 function createTextArea(hour, currentHour) {
-  const textArea = document.createElement('textarea');
+  var textArea = document.createElement('textarea');
   textArea.classList.add(getTextAreaBackgroundClass(hour, currentHour));
   return textArea;
 }
@@ -82,7 +93,7 @@ function getTextAreaBackgroundClass(hour, currentHour) {
 }
 
 function createSaveBtn(hour) {
-  const saveBtn = document.createElement('button');
+  var saveBtn = document.createElement('button');
   saveBtn.classList.add('saveBtn');
   saveBtn.innerHTML = '<i class="fas fa-save"></i>';
   saveBtn.setAttribute('data-hour', hour);
@@ -90,7 +101,7 @@ function createSaveBtn(hour) {
 }
 
 function appendTimeblockColumns(timeblockRow, hourCol, textAreaCol, saveBtnCol) {
-  const innerCols = [hourCol, textAreaCol, saveBtnCol];
+  var innerCols = [hourCol, textAreaCol, saveBtnCol];
   for (let col of innerCols) {
     timeblockRow.appendChild(col);
   }
@@ -99,8 +110,8 @@ function appendTimeblockColumns(timeblockRow, hourCol, textAreaCol, saveBtnCol) 
 // functions for saving to local storage //
 function containerClicked(event, timeblockList) {
   if (isSaveButton(event)) {
-    const timeblockHour = getTimeblockHour(event);
-    const textAreaValue = getTextAreaValue(timeblockHour);
+    var timeblockHour = getTimeblockHour(event);
+     var textAreaValue = getTextAreaValue(timeblockHour);
     placeTimeblockInList(new TimeblockObj(timeblockHour, textAreaValue), timeblockList);
     saveTimeblockList(timeblockList);
   }
